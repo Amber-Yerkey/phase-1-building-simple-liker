9fc41a9013b9168e65f3e06071d067002cf167ff
Amber Frantz <amber.k.frantz@mgail.com>
2022-05-08 11:26:31 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -4,8 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heart = document.getElementsByClassName('like-glyph')
+const errorBox = document.getElementById("modal")
+const errorMsg = document.getElementById("modal-message")
+errorBox.className = "hidden"
+//errorBox.hidden = true
 
+for (const button of heart) {
+  button.addEventListener('click', () => {
+//    e.preventDefault()
 
+    mimicServerCall()
+
+    .then(() => {
+      if(button.className === "activated-heart"){
+        button.textContent = EMPTY_HEART
+        button.classList.remove("activated-heart")
+      } else {
+        button.textContent = FULL_HEART
+        button.className = "activated-heart"
+      }
+    })
+
+    .catch((serverError) => {
+      //errorBox.className = false
+      errorBox.classList.remove("hidden")
+      errorMsg.textContent = serverError
+      setTimeout(() => {errorMsg.setAttribute('class', 'hidden')}, 3000)
+    })
+
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
